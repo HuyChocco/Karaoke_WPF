60d30b1ae88d845349185c93ff87dd9448468954
HuyChocco <14520385@gm.uit.edu.vn>
2018-10-19 20:44:40 +0700

    Upload code first

---
+++ QuanLyKaraoke/DataProvider.cs
@@ -0,0 +1,34 @@
+﻿using QuanLyKaraoke.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKaraoke
+{
+    public class DataProvider
+    {
+		private static DataProvider _ins;
+		public static DataProvider Ins
+		{
+			get
+			{
+				if (_ins == null)
+					_ins = new DataProvider();
+				return _ins;
+
+			}
+			set
+			{
+				_ins = value;
+			}
+		}
+		public QuanLyKaraokeEntities DB { get; set; }
+		public DataProvider()
+		{
+			DB = new QuanLyKaraokeEntities();
+		}
+
+	}
+}
---
+++ QuanLyKaraoke/ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace QuanLyKaraoke.ViewModel
+{
+    public class MainViewModel
+    {
+		public bool Isloaded = false;
+		public ICommand LoadedWindowCommand { get; set; }
+		public MainViewModel()
+		{
+			LoadedWindowCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+			{
+				Isloaded = true;
+				if (p == null)
+					return;
+				p.Hide();
+				LoginWindow loginWindow = new LoginWindow();
+				loginWindow.ShowDialog();
+
+				if (loginWindow.DataContext == null)
+					return;
+				var loginVM = loginWindow.DataContext as LoginViewModel;
+
+				if (loginVM.IsLogin)
+				{
+					p.Show();
+					//LoadTonKhoData();
+				}
+				else
+				{
+					p.Close();
+				}
+			}
+			);
+		}
+    }
+}
